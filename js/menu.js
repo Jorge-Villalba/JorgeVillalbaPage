58a1844e2a9929fe00e0e780b9f4653d75c17e7e
jorge-villalba <jorgevillalbadev@gmail.com>
2022-01-13 01:04:39 -0500

    Menu bar responsive update

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,32 @@
 const nav = document.querySelector('.nav');
+const menuMobile = document.querySelector('.menu-mobile');
+const menu = document.querySelector('.menu')
+const closeMenu = document.querySelector('.menu .close-menu');
+var menuOption = document.querySelectorAll('.menu-option'); // create an array
 
-window.addEventListener('scroll',function(){
-    nav.classList.toggle('active',window.scrollY > 0)
+window.addEventListener('scroll', function () {
+    nav.classList.toggle('active', window.scrollY > 0)
 })
+
+menuMobile.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
+
+menuOption.forEach(function(myBtn){
+    /*add a listener to every element of this array */
+    myBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    })
+});
+
+
+
+
+
+
+
+
